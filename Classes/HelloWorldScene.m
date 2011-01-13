15f474ece557491357b1ca96f2be2396c0aaa96c
Fernando Ribeiro <ferbass@gmail.com>
2011-01-13 17:40:46 -0200

    remove helloworldcode

--- Classes/HelloWorldScene.m
+++ Classes/HelloWorldScene.m
@@ -33,18 +33,7 @@
 	// always call "super" init
 	// Apple recommends to re-assign "self" with the "super" return value
 	if( (self=[super init] )) {
-		
-		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
 
-		// ask director the the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
-	
-		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
-		
-		// add the label as a child to this Layer
-		[self addChild: label];
 	}
 	return self;
 }
